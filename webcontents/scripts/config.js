ce4a4af1b34f693d96f1e59c45516cebe9d040cc
Amit Rai <amitrai@amits-air.home>
2024-07-17 10:30:04 -0400

    adding files to repo

---
+++ webcontents/scripts/config.js
@@ -0,0 +1,9 @@
+const config = {
+    envType: 'prod', // Change this value to 'prod' for production
+    userApiKey: 'add_your_key_for_user_api',
+    emailApiKey: 'add_your_key_for_email_api',
+    UserPoolId: 'add_user_pool_id', // Your user pool id here
+    ClientId: 'add_client_id' // Your client id here
+};
+
+export default config;
